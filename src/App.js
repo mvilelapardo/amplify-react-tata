321d1a2a2726bde85d06112445b575b14de3a1a4
mvilelapardo <mvilelapardo@gmail.com>
2024-05-09 23:28:06 +0200

    Correction on the titles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import YouTubeVideo from './YouTubeVideo.js';
 function App() {
   return (
     <div className="App">
-      <h1>VIDEO PASANTES 2024 MORENADA SAN GABRIEL</h1>
+      <h2 style={{ marginTop: '20px', color: 'white' }}>VIDEO PASANTES 2024 MORENADA SAN GABRIEL</h2>
       <YouTubeVideo videoId="x_Mi_yOa_Bo" />
       <ConstructionPage />
     </div>
