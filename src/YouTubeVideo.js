9b868f4e00d14bef2a8180b039c652642dcf3a0e
mvilelapardo <mvilelapardo@gmail.com>
2024-05-09 23:24:09 +0200

    Responsive configuration for the YouTubeVideo

--- src/YouTubeVideo.js
+++ src/YouTubeVideo.js
@@ -4,14 +4,17 @@ const YouTubeVideo = ({ videoId }) => {
     const src = `https://www.youtube.com/embed/x_Mi_yOa_Bo`;
 
     return (
-        <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/x_Mi_yOa_Bo?si=epl6GopVYmUgYGab&amp;controls=0"
-            title="YouTube video player" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen
-        ></iframe>
+        <div className="video-responsive">
+            <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/x_Mi_yOa_Bo?si=epl6GopVYmUgYGab&amp;controls=0"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen
+            ></iframe>
+        </div>
+        
     );
 };
 
