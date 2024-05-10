bd0cdc2f3bdbae619fd5b944f1e7d75ae8c89662
mvilelapardo <mvilelapardo@gmail.com>
2024-05-11 00:31:44 +0200

    Second video inserted

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import ConstructionPage from './ConstructionPage.js';
 import YouTubeVideo from './YouTubeVideo.js';
+import YouTubeVideo_2 from './YouTubeVideo_2.js';
 
 function App() {
   return (
     <div className="App">
       <h2 style={{ marginTop: '20px', color: 'white' }}>VIDEO PASANTES 2024 MORENADA SAN GABRIEL</h2>
       <YouTubeVideo videoId="x_Mi_yOa_Bo" />
+      <h2 style={{ marginTop: '20px', color: 'white' }}>RESEÑA HISTÓRICA DEL TATA SANTIAGO</h2>
+      <YouTubeVideo_2 videoId="jRrtdzcL0vg" />
       <ConstructionPage />
     </div>
   );
---
+++ src/YouTubeVideo_2.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const YouTubeVideo = ({ videoId }) => {
+    const src = `https://www.youtube.com/embed/jRrtdzcL0vg`;
+
+    return (
+        <div className="video-responsive">
+            <iframe 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/jRrtdzcL0vg?si=5qzYKYAMbcsPbJ-T" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen
+            ></iframe>
+        </div>
+        
+    );
+};
+
+export default YouTubeVideo;
