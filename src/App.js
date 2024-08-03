ceff2dcb3488914698487911a35e14ca84f24971
mvilelapardo <mvilelapardo@gmail.com>
2024-08-03 14:58:39 +0200

    Corrections on titles

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import YouTubeVideo_2VC from './YouTubeVideo_2VC.js';
 function App() {
   return (
     <div className="App">
-      <h2 className="title">DOCUMENTAL "TATA SANTIAGO" - VERSIÓN RESUMIDA</h2>
+      <h2 className="title">DOCUMENTAL "TATA SANTIAGO" - VERSIÓN RESUMIDA PARTE I y PARTE II</h2>
       <div className="video-wrapper">
         <YouTubeVideo_VR videoId="KzeU0id_miE" />
       </div>
@@ -22,13 +22,13 @@ function App() {
       <div className="video-section">
         <div className="video-pair">
           <div className="video-left">
-            <h2 className="title">PARTE I</h2>
+            <h2 className="title">PARTE I - HISTORIA DEL "APOSTOL SANTIAGO"</h2>
             <div className="video-wrapper">
               <YouTubeVideo_1VR videoId="RfMapUg4VA0" />
             </div>
           </div>
           <div className="video-right">
-            <h2 className="title">PARTE II</h2>
+            <h2 className="title">PARTE II - ENTREVISTA NARRATIVA A FUNDADORES DE LA FRATERNIDAD MORENADA "SAN GABRIEL"</h2>
             <div className="video-wrapper">
               <YouTubeVideo_2VR videoId="wyWbGfoxg5I" />
             </div>
@@ -36,7 +36,7 @@ function App() {
         </div>
       </div>
 
-      <h2 className="title">DOCUMENTAL "TATA SANTIAGO" - VERSIÓN EXTENDIDA</h2>
+      <h2 className="title">DOCUMENTAL "TATA SANTIAGO" - VERSIÓN EXTENDIDA PARTE I y PARTE II</h2>
       <div className="video-wrapper">
         <YouTubeVideo_VC videoId="DTCiGAmSWuw" />
       </div>
@@ -44,13 +44,13 @@ function App() {
       <div className="video-section">
         <div className="video-pair">
           <div className="video-left">
-            <h2 className="title">PARTE I</h2>
+            <h2 className="title">PARTE I - HISTORIA DEL "APOSTOL SANTIAGO</h2>
             <div className="video-wrapper">
               <YouTubeVideo_1VC videoId="eQdN4vU2AQM" />
             </div>
           </div>
           <div className="video-right">
-            <h2 className="title">PARTE II</h2>
+            <h2 className="title">PARTE II - ENTREVISTA NARRATIVA A FUNDADORES DE LA FRATERNIDAD MORENADA "SAN GABRIEL"</h2>
             <div className="video-wrapper">
               <YouTubeVideo_2VC videoId="knC4w4rrwQo" />
             </div>
