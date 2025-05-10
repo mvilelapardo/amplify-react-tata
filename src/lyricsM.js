231a86f086084654c3a88b22eff4e022422e21ae
Marcelo Vilela <mvilelapardo@gmail.com>
2025-05-10 22:22:22 +0200

    including ogg audio file and different html audio

--- src/lyricsM.js
+++ src/lyricsM.js
@@ -4,7 +4,10 @@ import tataImage from './tata01.png';
 function lyricsM() {
   return (
     <div>
-      <audio controls autoPlay src="/SanGabriel2025.mp3" />
+      <audio controls autoplay>
+        <source src="SanGabriel2025.ogg" type="audio/ogg" />
+        <source src="SanGabriel2025.mp3" type="audio/mpeg" />
+      </audio>
       <br></br>
       <img src={tataImage} alt="TATA" style={{ maxWidth: '80%', maxHeight: '150v' }} />
     </div>
