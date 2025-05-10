4a3667d9214186f9afa7f11ed7dcfdff6ca40fb6
Marcelo Vilela <mvilelapardo@gmail.com>
2025-05-10 22:30:37 +0200

    audio in src

--- src/lyricsM.js
+++ src/lyricsM.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import tataImage from './tata01.png';
+import tataAudio01 from './SanGabriel2025.ogg';
+import tataAudio02 from './SanGabriel2025.mp3';
 
 function lyricsM() {
   return (
     <div>
       <audio controls autoplay>
-        <source src="SanGabriel2025.ogg" type="audio/ogg" />
-        <source src="SanGabriel2025.mp3" type="audio/mpeg" />
+        <source src={tataAudio01} type="audio/ogg" />
+        <source src={tataAudio02} type="audio/mpeg" />
       </audio>
       <br></br>
       <img src={tataImage} alt="TATA" style={{ maxWidth: '80%', maxHeight: '150v' }} />
